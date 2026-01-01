143147da9273774a7b69cf8b210e1a3c3318bf5e
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-01 17:16:42 +0530

    Initial commit: React + Tailwind setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-red-600">
+        Netflix GPT
+      </h1>
+    </div>
+  );
+}
+
+export default App;
