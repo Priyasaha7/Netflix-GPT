76918e8f5652721aa704a46a47c7d700762df9f5
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-01-04 01:08:12 +0530

    Secondary Container with movielist , movie card and usepopular hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import Header from './Header'
-import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
-import MainContainer from './MainContainer';
-import SecondaryContainer from './SecondaryContainer';
+import React from "react";
+import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
 
 const Browse = () => {
-
-  useNowPlayingMovies(); 
+  
+  useNowPlayingMovies();
+  usePopularMovies();
 
   return (
-      <div>
-        <Header />
-        <MainContainer />
-        <SecondaryContainer />
-      </div>
-    );
-  };
+    <div>
+      <Header />
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
+  );
+};
 
 export default Browse;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { CDN_IMG_URL } from "../utils/constants.js";
+
+const MovieCard = ({ posterPath }) => {
+
+  if (!posterPath) return;
+
+  return (
+    <div className="w-40 pr-3">
+      <img src={CDN_IMG_URL + posterPath} alt="Movie card" />
+    </div>
+  );
+};
+
+export default MovieCard;
+
